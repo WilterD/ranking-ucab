7cbde482bc9ab48b72d6e08d12be76bbc93d8f38
WilterD <wilterd18@gmail.com>
2023-03-05 00:48:31 -0400

    config railway pacakage

--- src/database/db.cjs
+++ src/database/db.cjs
@@ -1,3 +1,11 @@
+import {
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME,
+    DB_PORT
+} from './config.js';
+
 const mysql = require('mysql');
 
 const conexion = mysql.createConnection({
